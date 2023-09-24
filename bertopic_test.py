f31f11c63246b63c4d5b72b8a480216e1758526c
alexF3 <42326198+alexF3@users.noreply.github.com>
2023-09-24 15:19:19 -0400

    md adjust

--- bertopic_test.py
+++ bertopic_test.py
@@ -33,7 +33,7 @@ st.write("It is important to note that the code generating these results makes n
 st.header("Try an experiment:") 
 st.write('In the search box below, type "marketing and advertising" to search this corpus of Oxycontin articles')
 st.markdown("Once you've evaluated the results, take a look at a [PubMed search](https://pubmed.ncbi.nlm.nih.gov/?term=%28%28oxycontin%29+AND+%28marketing%29%29+AND+%28advertising%29&sort=&filter=simsearch1.fha) for the same terms")
-st.markdown('**Which do you find more useful?**')
+st.markdown('> **Which do you find more useful?**')
 
 # Text input from the user
 user_text = st.text_area("Enter text you'd like to find Oxycontin articles about here:")
