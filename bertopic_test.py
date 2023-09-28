b74a0237265438071cee1121e0f3b555c2e3c6f8
alexF3 <42326198+alexF3@users.noreply.github.com>
2023-09-28 11:26:14 -0400

    added MD to about

--- bertopic_test.py
+++ bertopic_test.py
@@ -139,4 +139,62 @@ with tab1:
 
 with tab2:
 
-    st.title("Here's how all of this works:")
+    st.markdown("""# About this Demo
+
+### BERTopic Overview
+This demo uses a pre-trained topic model created using the [BERTopic](https://maartengr.github.io/BERTopic/index.html) Python library.  The library uses a set of modular tools to facilitate the use of language models from the [HuggingFace API](https://huggingface.co/models) in surfacing themes in free text corpora.
+
+Bertopic was developed by [Maarten Grootendorst](https://www.maartengrootendorst.com/) and introduced in [this](https://arxiv.org/abs/2203.05794) 2022 paper.  The most important thing that you need to know about BERTopic is that it's based on an NLP approach called Bidirectional Encoder Representations from Transformers (BERT) which was [introduced](https://arxiv.org/abs/1810.04805) by scientists from Google in 2018.  BERT differs from many other topic modeling approaches in that it retains the context before and after a given word.  Traditional NLP methodologies like Latent Derichlet Allocation (LDA) use a "bag-of-words" approach in which the document "Dog bites man" encodes the same way as the document "Man bites dog."  BERT avoids this problem.
+
+At a high level, BERTopic breaks down into a few steps:
+1. Transform the corpus of documents beings studied into vectors ("embeddings") using a language model the user selects
+2. Reduce the dimensionality of the resulting vectors
+3. Cluster the reduced vectors 
+4. Apply TF-IDF to the resulting topical clusters to weight the words that appear in each
+5. Use one or more representation models to select the words that best represent the themes in each cluster.
+
+Each of these steps involves a variety of algorithmic options and hyperparemeters and it's worthwhile to go through the documentation in detail.  
+
+### To me, there are two things that are especially exciting:
+
+First, in transforming the documents to embeddings (step 1), you can select any language model from the HuggingFace API.  That means that you can readily upgrade your code as new and improved language models become available.
+
+Second, the representation model phase (step 5) you can choose from a variety of different approaches to selecting the words that best define a thematic cluster of documents.  You have the option to provide a set of candidate topics and have the algorithm attempt to assign clusters to them.  If you're using an open or non-proprietary corpus you can even use the OpenAI API to provide summarizations of the documents in each topic. 
+
+BERTopic can be used to create modular, reusable pipelines that are readily tunable and applicable to a broad variety of tasks.  **Applications like this one could be hosted in secure VPCs or air gapped environments without access to the web.**
+
+## Overview of the approach behind this demo
+
+**Objective:**
+
+Imagine you're a senior FDA official overseeing panels of expert physicians who advise the administration on policy matters around specific drugs or devices.  You're not able to read every paper on every product, but you want to come into meetings with a sense of where the research around the given drug is in terms of themes and trends.  When faced with potentially dangerous drugs like Purdue Pharma's Oxycontin, you want to be sure that you have an independent sense of whether the broader medical research community is raising any alarms.
+
+You don't have time to read three thousand abstracts.  You want to rapidly get a sense of emerging themes over time.
+
+BERTopic can help with that!
+
+**Data:**
+
+A collection of 3,737 abstracts was drawn from [PubMed](https://pubmed.ncbi.nlm.nih.gov/) using the search term "Oxycontin."  These were downloaded as an .nbib file and reformatted as a Pandas dataframe using the nbib Python library.
+
+**Methods:**
+
+This app was developed using [Google Colab](https://colab.research.google.com) for free access to the GPUs needed to conduct embeddings relatively quickly.  The notebook used can be viewed and forked [here](https://github.com/alexF3/BERTopic_overview/blob/main/bertopic_research/BERTopic_demo_sourcecode_21sep23.ipynb).
+
+I used the sentence transformer "all-MiniLM-L6-v2" to produce embeddings of the abstracts in the corpus.  The BERT approach requires little to no pre-processing, which makes for fast development cycles.  I used the UMAP library to reduce the dimensions of the corpus embedding and the HDBSCAN library to cluster the resulting vectors with a minimum cluster size of 20 documents.  I then used the PartOfSpeech representation model to select the most important words in each topical cluster.
+
+I was able to save the resulting topic model without the embeddings, which came in at under 8 MB.  
+
+For the demo, I set up a Streamlit app to transform user input text in the same way that the embeddings the model is based on were transformed.  BERTopic's find_topics method is then used to find topics in the model with a probability of matching of at least 0.1 (this is tunable depending on whether you want to cast a wider or narrower net).  
+
+With matching topics captured, I then use the wordcloud library to generate a word cloud of the highest TF-IDF words in all of the documents in each topic.
+
+Next, I use metadata from each of the topic matched abstracts to created a plotly line chart of annual occurances of matched topics since Oxycontin's introduction in 1996.
+
+Finally, the titles and hyperlinks to PubMed pages for each matched article are presented.
+
+
+
+
+
+""")
