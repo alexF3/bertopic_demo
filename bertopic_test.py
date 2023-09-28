ea0232f41c3a6e42cc798f329718b18521fab6e3
alexF3 <42326198+alexF3@users.noreply.github.com>
2023-09-28 07:36:58 -0400

    Added "about" page

--- bertopic_test.py
+++ bertopic_test.py
@@ -18,119 +18,125 @@ topic_model = BERTopic.load("data/bertopic_model_oxycontin_pos",model)
 df = pd.read_csv('data/oxycontin_with_topics_pos.csv')
 df['date'] = pd.to_datetime(df['date'])
 
+tab1, tab2 = st.tabs(["Demo", "About"])
 
-# Streamlit App
-st.title("BERTopic Med Journal Article Theme Extraction")
+with tab1:
 
-st.image("data/forest_trees.jpg")
+    # Streamlit App
+    st.title("BERTopic Med Journal Article Theme Extraction")
 
-st.write("This is a demo of a topic modeling library using a pre-trained language model to surface articles with topics similar to user input text among a corpus of 3,747 abstracts drawn from a PubMed search for the word 'Oxycontin.'")
-st.markdown("You can read more about how this works in the [BERTopic Documentation](https://maartengr.github.io/BERTopic/index.html).")
-st.write("This approach often works better than more traditional NLP algorithms and requires a lot less pre-processing. Check out the results for yourself!")
+    st.image("data/forest_trees.jpg")
 
-## Demo example
-##
-st.header("Try an experiment:") 
-st.markdown('* Type "marketing and advertising" into the search box to explore the Oxycontin article corpus')
-st.markdown("* Compare thoes results to what you'd get from a [PubMed search](https://pubmed.ncbi.nlm.nih.gov/?term=%28%28oxycontin%29+AND+%28marketing%29%29+AND+%28advertising%29&sort=&filter=simsearch1.fha) for the Oxycontin marketing and advertising.")
-st.header('**Which do you find more useful?** :i_love_you_hand_sign:')
-st.write('(Hint: PubMed will give you 3 articles...this does better)')
+    st.write("This is a demo of a topic modeling library using a pre-trained language model to surface articles with topics similar to user input text among a corpus of 3,747 abstracts drawn from a PubMed search for the word 'Oxycontin.'")
+    st.markdown("You can read more about how this works in the [BERTopic Documentation](https://maartengr.github.io/BERTopic/index.html).")
+    st.write("This approach often works better than more traditional NLP algorithms and requires a lot less pre-processing. Check out the results for yourself!")
 
-# Text input from the user
-user_text = st.text_area("Enter text you'd like to find Oxycontin articles about here:")
+    ## Demo example
+    ##
+    st.header("Try an experiment:") 
+    st.markdown('* Type "marketing and advertising" into the search box to explore the Oxycontin article corpus')
+    st.markdown("* Compare thoes results to what you'd get from a [PubMed search](https://pubmed.ncbi.nlm.nih.gov/?term=%28%28oxycontin%29+AND+%28marketing%29%29+AND+%28advertising%29&sort=&filter=simsearch1.fha) for the Oxycontin marketing and advertising.")
+    st.header('**Which do you find more useful?** :i_love_you_hand_sign:')
+    st.write('(Hint: PubMed will give you 3 articles...this does better)')
 
-if user_text:
-    # Extract topics from the user-provided text
-    # topics, probs = topic_model.transform([user_text])
+    # Text input from the user
+    user_text = st.text_area("Enter text you'd like to find Oxycontin articles about here:")
 
-    topic, prob = topic_model.find_topics(user_text)
-    findings_frame = pd.DataFrame({'topic':topic,'prob':prob})
-    findings_frame = findings_frame[findings_frame.prob>0.1]
+    if user_text:
+        # Extract topics from the user-provided text
+        # topics, probs = topic_model.transform([user_text])
 
-    try:
-        st.header(f"BERTopic Search Found {len(df[df.topic.isin(findings_frame.topic.tolist())])} Articles with Similar Themes:")
-
-        ## Create wordcloud based on TF-IDF of each of the matching topic clusters
-        ##
-
-        st.set_option('deprecation.showPyplotGlobalUse', False)     
-        topic_wc, prob_wc = topic_model.find_topics(user_text)
-        findings_frame = pd.DataFrame({'topic_wc':topic_wc,'prob_wc':prob_wc})
-        findings_frame = findings_frame[findings_frame.prob_wc>0.1]
-
-        def create_wordcloud(model, topic_wc):
-            text = {}
-            for t in  topic_wc:
-                new_text =  {word: value for word, value in topic_model.get_topic(t)}
-                text = text | new_text
-            # text = {word: value for word, value in model.get_topic(5)}
-            wc = WordCloud(background_color="white", max_words=1000)
-            wc.generate_from_frequencies(text)
-            plt.imshow(wc, interpolation="bilinear")
-            plt.axis("off")
-            # plt.show()
-        wc_fig = create_wordcloud(topic_model, topic_wc=findings_frame.topic_wc.tolist())
-
-        ## Display word cloud
-        ##
-
-        st.write("Word cloud based on TF-IDF of the clusters of articles with themes that matched the entered text:")
-        st.pyplot(wc_fig)
-
-        ## Create line plot of articles from matching themes per year
-        ##
-
-        # Convert the date column to a pandas datetime object
-        df['date'] = pd.to_datetime(df['date'])
-
-        # Filter dataframe down to only entries in the topics that match the user_text
         topic, prob = topic_model.find_topics(user_text)
         findings_frame = pd.DataFrame({'topic':topic,'prob':prob})
         findings_frame = findings_frame[findings_frame.prob>0.1]
-        df = df[df.topic.isin(findings_frame.topic.tolist())]
 
-        # Extract the month and year from the date column
-        df['Year'] = df['date'].dt.year
+        try:
+            st.header(f"BERTopic Search Found {len(df[df.topic.isin(findings_frame.topic.tolist())])} Articles with Similar Themes:")
 
-        # Group by Year and count the rows
-        yearly_row_counts = df.groupby(['Year']).size().reset_index(name='Article Count')
+            ## Create wordcloud based on TF-IDF of each of the matching topic clusters
+            ##
 
-        # Create custom hover text with titles separated by line breaks for each year
-        def create_hover_text(year):
-            titles = df[df['Year'] == year]['title'].tolist()
-            titles_html = "<br>".join(html.escape(title) for title in titles)
-            return f"Year: {year}<br>Article Count: {yearly_row_counts[yearly_row_counts['Year'] == year]['Article Count'].values[0]}<br>Titles:<br>{titles_html}"
+            st.set_option('deprecation.showPyplotGlobalUse', False)     
+            topic_wc, prob_wc = topic_model.find_topics(user_text)
+            findings_frame = pd.DataFrame({'topic_wc':topic_wc,'prob_wc':prob_wc})
+            findings_frame = findings_frame[findings_frame.prob_wc>0.1]
 
-        yearly_row_counts['Hover Text'] = yearly_row_counts.apply(lambda row: create_hover_text(row['Year']), axis=1)
+            def create_wordcloud(model, topic_wc):
+                text = {}
+                for t in  topic_wc:
+                    new_text =  {word: value for word, value in topic_model.get_topic(t)}
+                    text = text | new_text
+                # text = {word: value for word, value in model.get_topic(5)}
+                wc = WordCloud(background_color="white", max_words=1000)
+                wc.generate_from_frequencies(text)
+                plt.imshow(wc, interpolation="bilinear")
+                plt.axis("off")
+                # plt.show()
+            wc_fig = create_wordcloud(topic_model, topic_wc=findings_frame.topic_wc.tolist())
 
-        # Create a line plot using Plotly Express with custom hover text and x-axis date range
-        fig = px.line(
-            yearly_row_counts,
-            x='Year',
-            y='Article Count',
-            title='In-Theme Article Counts by Year via BERTopic Search',
-            hover_data=['Hover Text']
-        )
+            ## Display word cloud
+            ##
 
-        # Set the x-axis date range to 1996 through 2023
-        fig.update_xaxes(title_text='Year', range=[1996, 2023])
-        fig.update_yaxes(range=[0,yearly_row_counts['Article Count'].max()+5])
+            st.write("Word cloud based on TF-IDF of the clusters of articles with themes that matched the entered text:")
+            st.pyplot(wc_fig)
 
-        fig.update_yaxes(title_text='Article Count')
+            ## Create line plot of articles from matching themes per year
+            ##
 
-        st.plotly_chart(fig, use_container_width=True)
+            # Convert the date column to a pandas datetime object
+            df['date'] = pd.to_datetime(df['date'])
 
-        ## Display article titles and pubmed links
-        ##
-        st.title("BERTopic App Search Results:")
-        st.markdown('([Click Here](https://pubmed.ncbi.nlm.nih.gov/?term=%28%28oxycontin%29+AND+%28marketing%29%29+AND+%28advertising%29&sort=&filter=simsearch1.fha) to see PubMed results for the demo search for oxycontin marketing and advertising.)')
-        st.write('')
-        st.write('')
-        for row in df[df.topic.isin(findings_frame.topic.tolist())].itertuples():
-            st.write(f"{row.title}")
+            # Filter dataframe down to only entries in the topics that match the user_text
+            topic, prob = topic_model.find_topics(user_text)
+            findings_frame = pd.DataFrame({'topic':topic,'prob':prob})
+            findings_frame = findings_frame[findings_frame.prob>0.1]
+            df = df[df.topic.isin(findings_frame.topic.tolist())]
 
-            st.write(f"https://pubmed.ncbi.nlm.nih.gov/{row.pubmed_id}")
+            # Extract the month and year from the date column
+            df['Year'] = df['date'].dt.year
 
-    except:
-        st.write("We're sorry.  There were no topics in the corpus similar enough to your input to display.")
+            # Group by Year and count the rows
+            yearly_row_counts = df.groupby(['Year']).size().reset_index(name='Article Count')
 
+            # Create custom hover text with titles separated by line breaks for each year
+            def create_hover_text(year):
+                titles = df[df['Year'] == year]['title'].tolist()
+                titles_html = "<br>".join(html.escape(title) for title in titles)
+                return f"Year: {year}<br>Article Count: {yearly_row_counts[yearly_row_counts['Year'] == year]['Article Count'].values[0]}<br>Titles:<br>{titles_html}"
+
+            yearly_row_counts['Hover Text'] = yearly_row_counts.apply(lambda row: create_hover_text(row['Year']), axis=1)
+
+            # Create a line plot using Plotly Express with custom hover text and x-axis date range
+            fig = px.line(
+                yearly_row_counts,
+                x='Year',
+                y='Article Count',
+                title='In-Theme Article Counts by Year via BERTopic Search',
+                hover_data=['Hover Text']
+            )
+
+            # Set the x-axis date range to 1996 through 2023
+            fig.update_xaxes(title_text='Year', range=[1996, 2023])
+            fig.update_yaxes(range=[0,yearly_row_counts['Article Count'].max()+5])
+
+            fig.update_yaxes(title_text='Article Count')
+
+            st.plotly_chart(fig, use_container_width=True)
+
+            ## Display article titles and pubmed links
+            ##
+            st.title("BERTopic App Search Results:")
+            st.markdown('([Click Here](https://pubmed.ncbi.nlm.nih.gov/?term=%28%28oxycontin%29+AND+%28marketing%29%29+AND+%28advertising%29&sort=&filter=simsearch1.fha) to see PubMed results for the demo search for oxycontin marketing and advertising.)')
+            st.write('')
+            st.write('')
+            for row in df[df.topic.isin(findings_frame.topic.tolist())].itertuples():
+                st.write(f"{row.title}")
+
+                st.write(f"https://pubmed.ncbi.nlm.nih.gov/{row.pubmed_id}")
+
+        except:
+            st.write("We're sorry.  There were no topics in the corpus similar enough to your input to display.")
+
+with tab2:
+
+    st.title("Here's how all of this works:")
